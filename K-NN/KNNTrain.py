29f8c74046e7988ded82bfde178c699424ffc3e5
Rafik Fawaz <61281330+rafikfawaz1@users.noreply.github.com>
2022-02-06 13:23:47 +0100

    Create KNNTrain.py

---
+++ K-NN/KNNTrain.py
@@ -0,0 +1,32 @@
+from cgi import test
+from tkinter.tix import Y_REGION
+import numpy as np 
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+
+iris= datasets.load_iris()
+X,y = iris.data, iris.target 
+
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2,random_state=1234)
+# print (X_train.shape)
+# print(X_train[0])
+# print(y_train)
+# plt.figure()
+# plt.scatter(X[:,0],X[:,1],c =y)
+# plt.show()
+
+
+## most common test 
+# a= [1,1,1,1,3,4,5,5,5]
+# from collections import Counter
+# most_common = Counter(a).most_common(1)
+# print(most_common[0][0])
+
+from KNN import KNN 
+clf = KNN(k=2)
+clf.fit(X_train,y_train)
+predictions = clf.predict(X_test)
+
+acc= np.sum(predictions==y_test)/len(y_test)
+print(acc)
